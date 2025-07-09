b6ab47c1067407f4ed2ee91d04023a8b0630da64
abidcc3 <abidkhan+2@codecraft.co.in>
2025-07-09 11:34:55 +0530

    Initial commit

---
+++ main.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:archive/archive_io.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Modal Dialog Demo', home: const HomeScreen());
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  void _showDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Hello World"),
+          content: const Text("This is a test dialog."),
+          actions: [
+            TextButton(
+              onPressed: () async {
+                Navigator.of(context).pop();
+              },
+              child: const Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // if your terminal doesn't support color you'll see annoying logs like `\x1B[1;35m`
+    return Scaffold(
+      appBar: AppBar(title: const Text('Modal Dialog Example')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => _showDialog(context),
+          child: const Text('Show Dialog'),
+        ),
+      ),
+    );
+  }
+}
